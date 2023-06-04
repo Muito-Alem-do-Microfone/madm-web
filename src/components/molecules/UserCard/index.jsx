3539158104a60de02941b8f92445a223bdcb9edb
gitcore33 <erickestanislau25@gmail.com>
2023-06-04 17:20:01 -0300

    cards sections almost finished/updating header

--- src/components/molecules/UserCard/index.jsx
+++ src/components/molecules/UserCard/index.jsx
@@ -1,9 +1,21 @@
+import styles from "./userCard.module.scss";
+import profileImg from "../../../assets/profileImg.jpg";
+import Button from "../../atoms/Button";
+
 function UserCard() {
-    return (
-        <div>
-        Card aqui
-        </div>
-    );
-  }
-  
-  export default UserCard;
+  return (
+    <div className={styles.userCard}>
+      <div className={styles.profileImage}>
+        <img className={styles.image} src={profileImg} alt="Foto do Perfil" />
+      </div>
+      <div className={styles.profileContent}>
+        <h2>Erick Santos</h2>
+        <p className={styles.userInfo}>Guitarrista de São Paulo - SP </p>
+        <p className={styles.userInfo}>Curte trap, hardcore e Costa Gold</p>
+      </div>
+      <Button variant="primary" text="Conectar" />
+    </div>
+  );
+}
+
+export default UserCard;
--- src/components/organisms/CardSection2/index.jsx
+++ src/components/organisms/CardSection2/index.jsx
@@ -1,25 +1,24 @@
-import UserCard from "../../molecules/UserCard"
-import styles from "../CardSection1/cardSection.module.scss";
+import UserCard from "../../molecules/UserCard";
+import styles from "./cardSection2.module.scss";
 
 function CardSection2() {
-    return (
-        <>
-            <section>
+  return (
+    <>
+      <section>
+        <div className={styles.cardTitle}>
+          <p>Quem já está querendo formar banda perto de você?</p>
+        </div>
 
-                <div className={styles.cardTitle}>
-                    <p>Quem já está querendo formar banda perto de você?</p>
-                </div>
+        <div className={styles.userCardSection}>
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+          <UserCard />
+        </div>
+      </section>
+    </>
+  );
+}
 
-                <div>
-                    <UserCard />
-                    <UserCard />
-                    <UserCard />
-                    <UserCard />
-                </div>
-                    
-            </section>
-        </>
-    );
-  }
- 
-  export default CardSection2;
+export default CardSection2;
